98f4c9e4ded2abc0513dcb67b71b132fdbbdb37b
Alistair Willis <alistairrwillis@gmail.com>
2017-04-12 21:29:27 +0900

    add form model

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Employee } from "../models/employee.models";
 
 @Component({
   selector: 'home',
@@ -6,4 +7,14 @@ import {Component} from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  languages: string[] = ['English', 'Spanish', 'German', 'Other'];
+  model = new Employee('', '', false, 'w2', 'English');
+
+  firstNameToUpperCase(value: string): void {
+    if (value.length > 0) {
+      this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+    } else {
+      this.model.firstName = value;
+    }
+  }
 }
