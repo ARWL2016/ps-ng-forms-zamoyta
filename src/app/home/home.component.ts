97392f6d76db4573adb784a0b0f10db655785286
Alistair Willis <alistairrwillis@gmail.com>
2017-04-13 11:23:54 +0900

    add form validation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,11 +10,4 @@ export class HomeComponent {
   languages: string[] = ['English', 'Spanish', 'German', 'Other'];
   model = new Employee('', '', false, 'w2', 'English');
 
-  firstNameToUpperCase(value: string): void {
-    if (value.length > 0) {
-      this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
-    } else {
-      this.model.firstName = value;
-    }
-  }
 }
